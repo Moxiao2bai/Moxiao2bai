91e12a66ae0b5ca0f49a64279d59c12045f24db0
Moxiao2bai <842809762@qq.com>
2022-05-08 11:53:28 +0800

    儿子牛逼

---
+++ assets/js/login1.js
@@ -0,0 +1,68 @@
+$(function () {
+  $('#link_reg').click(function () {
+    $('.reg-box').show()
+    $('.login-box').hide()
+  })
+  $('#link_login').click(function () {
+    $('.reg-box').hide()
+    $('.login-box').show()
+  })
+  // 校验
+  // layui.use(['form', 'layer'], function () {
+
+  var form = layui.form;
+  var layer = layui.layer
+  form.verify({
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    repwd: function (value) {
+      var pwd = $('.reg-box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+      }
+    }
+  })
+  // })
+  // 注册
+  $('#form_reg').on('submit', function (e) {
+   
+    e.preventDefault()
+    
+
+    $.ajax({
+      type: "POST",
+      url: '/api/reguser',
+      data: {
+        username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+      },
+      
+      success: function (response) {
+        if(response.status !==0){
+         return layer.msg('shibai');
+        }
+        layer.msg('注册成功，请登录！')
+        $('#link_login').click()
+      }
+    });
+  })
+
+  // 登录
+  $('#form_login').on('submit', function (e) {
+    e.preventDefault()
+    
+    $.ajax({
+      type:'post',
+      url: '/api/login',
+      data:$(this).serialize(),
+      success: function(tgg){
+        if(tgg.status  !==0){
+          return layer.msg('shibai');
+        }
+        layer.msg('登陆成功！')
+        location.href="https://www.baidu.com/"
+      }
+    })
+
+  })
+})
+
---
+++ assets/js/tihuan.js
@@ -0,0 +1,3 @@
+$.ajaxPrefilter(function(options){
+    options.url ="http://www.liulongbin.top:30077"+options.url
+})
